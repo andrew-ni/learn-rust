d53391b65cb480ba11dca8bf989959415c2bffd2
Andrew Ni <niandrewdev@gmail.com>
2020-08-29 01:27:19 -0700

    Add enums

---
+++ enums/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let home = IpAddress::V4(127, 0, 0, 1);
+    let loopback = IpAddress::V6(String::from("::1"));
+}
+
+enum IpAddress {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+fn route(ip_kind: IpAddress) {}
